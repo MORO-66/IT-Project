570c72f89643f266580929893fb673377e120925
MohamedHegazy29 <121262195+MohamedHegazy29@users.noreply.github.com>
2023-04-14 22:34:27 +0200

    contact

--- contact.php
+++ contact.php
@@ -6,7 +6,22 @@
 </head>
 <body>
     <?php include "navbar.php"; ?>
-
+    <h2>Customer</h2>
+    <h1>Feedback</h1>
+    <p>Thank you for using the QMotors on your journey to modern marketing how can we do better? </p>
+    <p>let us know your thoughts</p>
+    <div class="row row-signup"> 
+        <div class=" col-lg-6 col-md-6 col-sm-8"> 
+            <label class="label">Frist name: </label><br>
+            <label class="label">Last name: </label><br>
+            <label class="label">E-mail: </label><br>
+            <label class="label">Feedback: </label><br>
+            <input class="input form-control" type="text" name="Frist name" value="" required>
+            <input class="input form-control" type="text" name="Last name" value="" required>
+            <input class="input form-control" type="email" name="E_mail" value="" required>
+            <input class="input form-control" type="" name="Feedback" value="" required>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
     <?php include "cta+footer.php"; ?>
 </body>
 </html>
