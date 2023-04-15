5ac71895b4a99ec5d39241647cc6936bad962904
MohamedHegazy29 <121262195+MohamedHegazy29@users.noreply.github.com>
2023-04-15 02:13:59 +0200

    تمام بس الصور الفي slide show  الفي cars  مش  بتزهر

--- cars.php
+++ cars.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <head>
         <?php include "head.php"; ?>
+        <link rel="stylesheet" href="./css/cars.css">
         <title>Qmotors</title>
     </head>
     <body>
@@ -12,45 +13,51 @@
         <section id="content-image">
             <div class="row">
                 <div class="col-lg-6">
-                    <img class="img-section" src="./images/bmw.jpg" alt="" width="42" height="52" style="vertical-align:top;margin:15px 20px">
+                    <img class="img-section" src="./images/bmw.jpg" alt="">
                 </div>
-                <div class="col-lg-6" >
-                    <p>Car model: </p>
-                    <p>Price: </p>
-                    <p>About:<!--comment about the car --></p>
+                <div class="col-lg-6" style="text-align: left;" >
+                    <h3>Car model:</h3><br> 
+                    <h3>Price: </h3><br>
+                    <h3>About:<!--comment about the car --></h3>
                 </div>
             </div>
         </section>
+        <section>
+        <div class="slideshow-container">
 
-        <section id="slide-content">
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000" data-pause="hover" data-keyboard="true">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img class="img-section" src="./images/best_luxury_car.jpg" alt="">
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img class="img-section" src="./images/bmw.jpg" alt="">
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img class="img-section" src="./images/WhatsApp Image 2023-04-10 at 12.25.42 PM.jpeg" alt="">
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img class="img-section" src="./images/car-nissan-race-cars-road-wallpaper-preview.jpg" alt="">
-            </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-        </section>
-        
+        <!-- Full-width images with number and caption text -->
+        <div class="mySlides fade">
+            <div class="numbertext">1 / 3</div>
+            <img class="testimonial-image img-section" src="./images/WhatsApp Image 2023-04-10 at 12.25.38 PM.jpeg" style="width:100%">
+            <div class="text">Caption Text</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 3</div>
+            <img class="testimonial-image img-section" src="./images/bmw.jpg" style="width:100%">
+            <div class="text">Caption Two</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 3</div>
+            <img class="testimonial-image img-section" src="./images/bmw2.jpg" style="width:100%">
+            <div class="text">Caption Three</div>
+        </div>
+
+        <!-- Next and previous buttons -->
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        </div>
+        <br>
+
+        <!-- The dots/circles -->
+        <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+        </div>
+    </section>
         <?php include "cta+footer.php"; ?>
     </body>
+
 </html>
