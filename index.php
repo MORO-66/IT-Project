e9b1bf03d5389edb34d1d38b6de80f26a221fcef
kareem ehab <karemehab2003@gmail.com>
2023-04-18 01:36:18 +0200

    Index.php Updated

--- index.php
+++ index.php
@@ -59,94 +59,31 @@ check_login($db)
     <div>
       <div>
         <div style="display: flex; justify-content: space-between; margin: 15px 5%; flex-wrap: wrap; gap: 10px;">
-          <div class="card__body">
-            <div class="card__body-cover">
-              <img src="./images/WhatsApp Image 2023-04-10 at 12.25.38 PM.jpeg" alt="">
-            </div>
-            <header class="card__body-header">
-              <p class="card__body-header-subtitle">Nissan Mustang : A Car that Every Woman need</p>
-            </header>
-            <h2>20000$</h2>
-          </div>
-          <div class="card__body">
-            <div class="card__body-cover">
-              <img src="./images/WhatsApp Image 2023-04-10 at 12.25.39 PM (1).jpeg" alt="">
-            </div>
-            <header class="card__body-header">
-              <p class="card__body-header-subtitle">Rolls Royce Phantom : A Work Of Art</p>
-            </header>
-            <h2>30000$</h2>
-          </div>
-          <div class="card__body">
-            <div class="card__body-cover">
-              <img src="./images/WhatsApp Image 2023-04-10 at 12.25.39 PM (2).jpeg" alt="">
-            </div>
-            <header class="card__body-header">
-              <p class="card__body-header-subtitle">Porsche 911 Carrera : Engineered for magic </p>
-            </header>
-            <h2>25000$</h2>
-          </div>
-          <div class="card__body">
-            <div class="card__body-cover">
-              <img src="./images/WhatsApp Image 2023-04-10 at 12.25.39 PM.jpeg" alt="">
-            </div>
-            <header class="card__body-header">
-              <p class="card__body-header-subtitle">Mercedes Benz A-Class : The Peak Of Luxury</p>
-            </header>
-            <h2>27500$</h2>
-          </div>
-          <div class="card__body">
-            <div class="card__body-cover">
-              <img src="./images/WhatsApp Image 2023-04-10 at 12.25.40 PM.jpeg" alt="">
-            </div>
-            <header class="card__body-header">
-              <p class="card__body-header-subtitle">Mopar Jeep : Proud Sponsors Of Mud</p>
-            </header>
-            <h2>26500$</h2>
-          </div>
-          <div>
-            <div class="card__body">
-              <div class="card__body-cover">
-                <img src="./images/WhatsApp Image 2023-04-10 at 12.25.41 PM (1).jpeg" alt="">
-              </div>
-              <header class="card__body-header">
-                <p class="card__body-header-subtitle">MG ZS : Take Charge </p>
-              </header>
-              <h2>150$ (Only For Rent)</h2>
-            </div>
-          </div>
-          <div>
-            <div class="card__body">
-              <div class="card__body-cover">
-                <img src="./images/WhatsApp Image 2023-04-10 at 12.25.41 PM.jpeg" alt="">
-              </div>
-              <header class="card__body-header">
-                <p class="card__body-header-subtitle"> Audi Prollogue Avant : Best Midrange Car</p>
-              </header>
-            <h2>11000$</h2>
-            </div>
-          </div>
-          <div>
-            <div class="card__body">
-              <div class="card__body-cover">
-                <img src="./images/WhatsApp Image 2023-04-10 at 12.25.42 PM (1).jpeg" alt="">
-              </div>
-              <header class="card__body-header">
-                <p class="card__body-header-subtitle">BMW : Best Seller Car</p>
-              </header>
-            <h2>28250$</h2>
-            </div>
-          </div><div>
-            <div class="card__body">
-              <div class="card__body-cover">
-                <img src="./images/WhatsApp Image 2023-04-10 at 12.25.42 PM.jpeg" alt="">
-              </div>
-              <header class="card__body-header">
-                <p class="card__body-header-subtitle">Toyota CHRm :Best Rental Car</p>
-              </header>
-              <h2>600$ (Only For Rent )</h2>
-            </div>
-          </div>
+        <?php
+
+                    $sql = "SELECT Name , Photo , Price From car LIMIT 6";
+                    $result = mysqli_query($db , $sql);
+
+                    while($row = mysqli_fetch_assoc($result)){
+                        
+                      echo "
+                      <div class='card__body'>
+                      <div class='card__body-cover'>
+                        <img src='".$row['Photo']."' alt=''>
+                      </div>
+                      <header class='card__body-header'>
+                        <p class='card__body-header-subtitle'>".$row['Name']."</p>
+                      </header>
+                      <h2>".$row['Price']. "$" . "</h2>
+                    </div>
+                      
+                      
+                      ";
+
+
+                    }
+
+                    ?>  
         </div>
       </div>
     </div>
