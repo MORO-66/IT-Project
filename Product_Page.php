eadb75bbb7a5cfd6bfbbca344839a2d1f03a2b23
Mohamed Tarek <43273151+mtarek2005@users.noreply.github.com>
2023-04-23 05:46:06 +0200

    added a search box using javascript

--- Product_Page.php
+++ Product_Page.php
@@ -17,43 +17,55 @@ check_login($db)
 
     <!--Cars-->
     <div>
+      <input class="form-control" type="search" id="searchbox" oninput="search()" placeholder="Search..."/>
       <div>
-        <div style="display: flex; justify-content: space-between; margin: 15px 5%; flex-wrap: wrap; gap: 10px;">
-        <?php
-
-              $sql = "SELECT Name , Photo , Price , Id From car";
-              $result = mysqli_query($db , $sql);
-              
-        
-              while($row = mysqli_fetch_assoc($result)){
-                        
-                echo "
-                <a href = 'cars.php?Id=" . $row['Id']. "' style = 'text-decoration: none;' >
-                <div class='card__body'>
-                <div class='card__body-cover'>
-                  <img src='".$row['Photo']."' alt=''>
-                </div>
-                <header class='card__body-header'>
-                  <p class='card__body-header-subtitle'>".$row['Name']."</p>
-                </header>
-                <h2>".$row['Price']. "$" . "</h2>
-              </div>
-                </a>
-                
-                ";
-
-
-              }
-
-        ?>  
-
-
-
-
+        <div style="display: flex; justify-content: space-between; margin: 15px 5%; flex-wrap: wrap; gap: 10px;" id="main-cont">
 
         </div>
       </div>
     </div>
+    <script>
+    let cars=[
+      <?php
+
+              $sql = "SELECT Name , Photo , Price , Id From car";
+              $result = mysqli_query($db , $sql);
+
+
+              while($row = mysqli_fetch_assoc($result)){
+
+                echo "{Id:'" . $row['Id']. "',Photo:'".$row['Photo']."',Name:'".$row['Name']."',Price:'".$row['Price']. "'},";
+
+
+              }
+
+        ?>
+    ];
+    function get_item(data){
+      return " \
+                <a href = 'cars.php?Id=" + data['Id']+ "' style = 'text-decoration: none;' > \
+                <div class='card__body'> \
+                <div class='card__body-cover'> \
+                  <img src='"+data['Photo']+"' alt=''> \
+                </div> \
+                <header class='card__body-header'> \
+                  <p class='card__body-header-subtitle'>"+data['Name']+"</p> \
+                </header> \
+                <h2>"+data['Price']+ "$</h2> \
+              </div> \
+                </a>";
+    }
+    for(car of cars){
+      document.getElementById("main-cont").innerHTML+=get_item(car);
+    }
+    function search(){
+      let text = document.getElementById("searchbox").value;
+      document.getElementById("main-cont").innerHTML="";
+      for(car of cars){
+        if(car.Name.includes(text)) document.getElementById("main-cont").innerHTML+=get_item(car);
+      }
+    }
+    </script>
     <?php include "cta+footer.php"; ?>
   </body>
 </html>      
