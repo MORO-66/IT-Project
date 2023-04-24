ec88a87b983effcf4b79ded0279a78a2532c5787
Mohamed Tarek <43273151+mtarek2005@users.noreply.github.com>
2023-04-25 01:47:34 +0200

    change cascade order

--- head.php
+++ head.php
@@ -1,9 +1,9 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="./css/project-A.css">
 <link rel="stylesheet" href="./css/bootstrap.css">
 <link rel="stylesheet" href="./css/all.min.css">
+<link rel="stylesheet" href="./css/project-A.css">
 <link rel="icon" href="./images/LOGO.ico">
 <link rel="stylesheet" href="./css/Product_Page.css">
 <!-- js bootstrap -->
