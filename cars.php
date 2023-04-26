0d03ef765aeda1ba92720290b7ce2b115d56eb74
Mohamed Tarek <43273151+mtarek2005@users.noreply.github.com>
2023-04-26 21:26:20 +0200

    Revert "Update cars.php"
    This reverts commit 8f7ef3eb24be121111c6878b9569be38a801d835.

--- cars.php
+++ cars.php
@@ -50,7 +50,7 @@
         </div>
         <br>
 
-        <!-- The dots/circles -->  
+        <!-- The dots/circles -->
         <div style="text-align:center">
         <span class="dot" onclick="currentSlide(1)"></span>
         <span class="dot" onclick="currentSlide(2)"></span>
